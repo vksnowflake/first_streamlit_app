ece1e7fae0f573627dc6152cf2a4c2ba7c7d8545
Vanitha k <120738213+vksnowflake@users.noreply.github.com>
2022-12-19 14:44:53 +0530

    Update streamlit_app.py
    Modified Smoothie to Rocket smoothie from orange smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Orange Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
